f7d804935f1f7a683b63756b98521f544d853a09
awadhwana <24ankitw@gmail.com>
2018-11-13 18:18:57 +0530

    errors updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   before_action :user_find, only: [:show, :edit, :update, :destroy]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   def index
-    @users = User.paginate(page: params[:page], per_page: 3)
-    
+    @users = User.paginate(page: params[:page], per_page: 3)   
   end
   def show
    
@@ -19,7 +18,8 @@ class UsersController < ApplicationController
              
   end
 
-  def create     
+  def create
+      @user = User.new(user_params) 
       if @user.save
         session[:user_id] = @user.id
         flash[:success] = "Account created successfully!"
