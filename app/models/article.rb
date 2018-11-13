a37b874f60f0eedd9f89c1c1493721cb7e4e8400
awadhwana <24ankitw@gmail.com>
2018-11-13 18:24:58 +0530

    article description changed

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
     belongs_to :user
     
     validates :title, presence: true, length: {minimum:3,maximum:10}
-    validates :description, presence: true,length: {minimum:3,maximum:100}
+    validates :description, presence: true,length: {minimum:3,maximum:500}
     validates :user_id, presence: true
     
 end
